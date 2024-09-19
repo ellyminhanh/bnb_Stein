3ce74ca93122d4ef3e031b3fe3190428174c80c0
John Doe <minhanhhp1991@gmail.com>
2024-09-19 17:48:04 +0700

    categories menu

--- frontend/bnbclone/app/components/Categories.tsx
+++ frontend/bnbclone/app/components/Categories.tsx
@@ -1,7 +1,57 @@
+import Image from "next/image";
+
 const Categories = () => {
     return (
         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">tesststtst</div>
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover: border-gray-200 hover:opacity-100">
+                <Image 
+                    src = "/icn_icon.png"
+                    alt = "icons"
+                    width = {20}
+                    height={20}>
+                </Image>
+
+                <span className="text-xs">Newest</span>
+
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover: border-gray-200 hover:opacity-100">
+                <Image 
+                    src = "/icn_bungee.png"
+                    alt = "icons"
+                    width = {20}
+                    height={20}>
+                </Image>
+
+                <span className="text-xs">Bungee</span>
+
+            </div>
+
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover: border-gray-200 hover:opacity-100">
+                <Image 
+                    src = "/icn_rooom.png"
+                    alt = "icons"
+                    width = {20}
+                    height={20}>
+                </Image>
+
+                <span className="text-xs">Room</span>
+
+            </div>    
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover: border-gray-200 hover:opacity-100">
+                <Image 
+                    src = "/icn_villa.png"
+                    alt = "icons"
+                    width = {20}
+                    height={20}>
+                </Image>
+
+                <span className="text-xs">Villa</span>
+
+            </div> 
+            
         </div>
     )    
 }
