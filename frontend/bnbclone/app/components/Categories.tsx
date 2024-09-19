0f14c841846a75d1d026e5e000ccc5db5756fa0a
John Doe <minhanhhp1991@gmail.com>
2024-09-19 22:09:26 +0700

    update categories with full image

--- frontend/bnbclone/app/components/Categories.tsx
+++ frontend/bnbclone/app/components/Categories.tsx
@@ -30,7 +30,7 @@ const Categories = () => {
 
             <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover: border-gray-200 hover:opacity-100">
                 <Image 
-                    src = "/icn_rooom.png"
+                    src = "/icn_room.png"
                     alt = "icons"
                     width = {20}
                     height={20}>
