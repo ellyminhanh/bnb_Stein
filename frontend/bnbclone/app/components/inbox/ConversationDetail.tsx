ac0f3d97721ef4e51d8cb9eb0d05bbe9d5b03d2c
John Doe <minhanhhp1991@gmail.com>
2024-09-23 17:27:00 +0700

    conversation UIUX, click button

---
+++ frontend/bnbclone/app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import CustomButton from "../forms/CustomButton";
+
+const ConversationDetail = () => {
+    return (
+    <>
+        <div className="max-h-[480px] overflow-auto flex flex-col space-y-4">
+            <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-300">
+                <p className="font-bold text-gray-500">Elly</p>
+                <p>bjkasfdbsalkjfbsbcdu</p>
+            </div>
+
+            <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-blue-300">
+                <p className="font-bold text-gray-500">Anhnm</p>
+                <p>bjkasfdbsalkjfbsbcdu</p>
+            </div>
+
+        </div>
+
+        <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+            <input
+                fill 
+                type="text"
+                placeholder="Type your message..."
+                className="w-full p-2 bg-gray-200 rounded-xl">
+            </input>
+
+            <CustomButton 
+                label = 'Send'
+                onClick={()=> console.log("Clicked")}
+                className = "w-[100px]"
+            />
+        </div>
+
+    </>
+    )
+}
+
+export default ConversationDetail;
