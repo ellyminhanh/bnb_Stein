fa65e5ba0f38ce62b9cb2865f0e5cf5da5c75f18
John Doe <minhanhhp1991@gmail.com>
2024-09-23 17:37:48 +0700

    create button but havent create menu for button

--- frontend/bnbclone/app/components/Navbar/UserNav.tsx
+++ frontend/bnbclone/app/components/Navbar/UserNav.tsx
@@ -1,4 +1,13 @@
+'use client';
+
+import { useState } from "react";
+
+
+
 const UserNav = () => {
+
+    const [isOpen, setIsOpen] =useState (true)
+
     return (
         <div className="p-2 relative inline-block border rounded-full">
             <button className="flex items-center">
@@ -22,8 +31,16 @@ const UserNav = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
 
-
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex-col cursor-pointer">
+                    <p>sdkjvasv</p>
+                    <p>sdkjvasv</p>
+                    <p>sdkjvasv</p>
+                </div>
+            )} 
+
         </div>
     )
 }
