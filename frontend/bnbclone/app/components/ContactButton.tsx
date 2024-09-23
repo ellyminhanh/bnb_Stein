2b599f0da273cef51dbfc3925e2ac68c88236504
John Doe <minhanhhp1991@gmail.com>
2024-09-23 14:40:22 +0700

    finish lanalord page

---
+++ frontend/bnbclone/app/components/ContactButton.tsx
@@ -0,0 +1,10 @@
+const ContactButton = () => {
+    return (
+        <div className="mt -6 cursor-pointer py-4 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+            Contact  
+        </div>
+    )
+}
+
+
+export default ContactButton
