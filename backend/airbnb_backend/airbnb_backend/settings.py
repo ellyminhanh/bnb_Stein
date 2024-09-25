988ddb954137a3f802f3933695e0b4462447cd6d
John Doe <minhanhhp1991@gmail.com>
2024-09-25 16:13:42 +0700

    finish config, migreate python

--- backend/airbnb_backend/airbnb_backend/settings.py
+++ backend/airbnb_backend/airbnb_backend/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         "USER": os.environ.get("SQL_USER"),
         "PASSWORD": os.environ.get("SQL_PASSWORD"),
         "HOST": os.environ.get("SQL_HOST"),
-        'PORT': os.environ.get("SQL_PORT")
+        'PORT': os.environ.get("SQL_PORT"),
     }
 }
 
